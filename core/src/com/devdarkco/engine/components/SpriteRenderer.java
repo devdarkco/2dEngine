50c4f0f9ad7a5d5f2207e4948f04fa60e169d50c
devdarkco <devdarkco@gmail.com>
2019-04-03 22:34:14 +0100

    Refactored SpriteRenderer component.

--- core/src/com/devdarkco/engine/components/SpriteRenderer.java
+++ core/src/com/devdarkco/engine/components/SpriteRenderer.java
@@ -1,35 +1,22 @@
 package com.devdarkco.engine.components;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 
 public class SpriteRenderer extends Component {
 
+    private String texturePath;
     private Texture texture;
     private Sprite sprite;
-    private Vector2 spritePos;
+    public boolean render = false;
 
-    private String texturePath;
-
-    public SpriteRenderer(String texturePath, float x, float y) {
-        super();
-        this.texturePath = texturePath;
-        this.texture = new Texture(texturePath);
-        this.sprite = new Sprite(texture);
-        this.spritePos = new Vector2(x, y);
+    public SpriteRenderer(String texturePath){
+        texture = new Texture(texturePath);
+        sprite = new Sprite(texture);
     }
 
-    public void render(SpriteBatch batch, float x, float y) {
-        batch.draw(this.texture, x, y);
-    }
-
-    public String getTexturePath(){
-        return texturePath;
-    }
-
-    public Vector2 getSpritePos() {
-        return spritePos;
+    public Sprite getSprite() {
+        return sprite;
     }
 }
