85c4a9874a79b9ef0772f5259d9e9417609985f6
devdarkco <devdarkco@gmail.com>
2019-04-03 22:34:57 +0100

    Now Transform component works like a data holder

--- core/src/com/devdarkco/engine/components/Transform.java
+++ core/src/com/devdarkco/engine/components/Transform.java
@@ -1,21 +1,9 @@
 package com.devdarkco.engine.components;
 
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 
-public class Transform extends   Component{
+public class Transform extends Component {
 
-    private Vector2 position, scale;
-    public float x, y;
-
-    public Transform(float x, float y, float w, float h){
-        super();
-        this.position = new Vector2(x, y);
-        this.scale = new Vector2(w, h);
-        this.x = position.x;
-        this.y = position.y;
-    }
-
-    public Vector2 getPos(){
-        return this.position;
-    }
+    public Vector3 position = new Vector3();
+    public Vector3 scale = new Vector3(1, 1, 1);
 }
