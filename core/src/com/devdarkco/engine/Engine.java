fcb409eaa49097404f12f6058d9bee0c9d67b139
devdarkco <devdarkco@gmail.com>
2019-04-03 22:35:31 +0100

    Started using Main class to hold universal objects

--- core/src/com/devdarkco/engine/Engine.java
+++ core/src/com/devdarkco/engine/Engine.java
@@ -1,33 +1,30 @@
 package com.devdarkco.engine;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
 
-public class Engine extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class Engine {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private static World world;
+    private static OrthographicCamera camera;
+
+    public static void init(){
+        world = new World(new Vector2(0, -9.8f), false);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    }
+
+    public static void update(){
+        world.step(1/60f, 6, 2);
+    }
+
+    public static World getWorld() {
+        return world;
+    }
+
+    public static OrthographicCamera getCamera() {
+        return camera;
+    }
 }
